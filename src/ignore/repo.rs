71334fcb85d2b75a632cd41a7afa00c425d688f0
6607changchun <luobojiaozi@163.com>
2026-03-19 17:06:37 +0800

    print filelist

--- src/ignore/repo.rs
+++ src/ignore/repo.rs
@@ -154,6 +154,9 @@ mod tests {
         File::create(root.join("debug.log")).unwrap();
 
         let filter = RepoFilter::new(root).expect("should create filter");
+
+        println!("{:?}", filter.filelist());
+
         assert!(
             filter
                 .filelist()
