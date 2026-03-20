b7478dd22ac6ca213e929ea71c3e0883aaa7dcc5
6607changchun <84566142+6607changchun@users.noreply.github.com>
2026-03-20 15:14:41 +0800

    feat: repalce space to linebreak. (#7)

--- src/ignore/execute.rs
+++ src/ignore/execute.rs
@@ -2,6 +2,6 @@ use crate::ignore::repo::FilterList;
 
 pub fn run_filter(target: Vec<String>) {
     let multilist = FilterList::new(target);
-    let final_result = multilist.get_actual().join(" ");
+    let final_result = multilist.get_actual().join("\n");
     print!("{}", final_result);
 }
